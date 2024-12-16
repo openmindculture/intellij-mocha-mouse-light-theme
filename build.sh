db8d5cd2a389ee5cf507e6d4bb4c4e7d91e2d20b
Ingo Steinke <ingosteinke@gmx.de>
2024-12-16 19:47:26 +0100

    beta version 0.1.0

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
-mkdir build/intellij-mocha-mouse-dark-theme
-rm -rf build/intellij-mocha-mouse-dark-theme/*
-mkdir build/intellij-mocha-mouse-dark-theme/lib
-mkdir build/intellij-mocha-mouse-dark-theme-$1
-cp -r src/main/resources/* build/intellij-mocha-mouse-dark-theme-$1/
+mkdir build/intellij-mocha-mouse-light-theme
+rm -rf build/intellij-mocha-mouse-light-theme/*
+mkdir build/intellij-mocha-mouse-light-theme/lib
+mkdir build/intellij-mocha-mouse-light-theme-$1
+cp -r src/main/resources/* build/intellij-mocha-mouse-light-theme-$1/
 cd build
-cd intellij-mocha-mouse-dark-theme-$1
-zip -r ../intellij-mocha-mouse-dark-theme/lib/intellij-mocha-mouse-dark-theme-$1.jar ./*
+cd intellij-mocha-mouse-light-theme-$1
+zip -r ../intellij-mocha-mouse-light-theme/lib/intellij-mocha-mouse-light-theme-$1.jar ./*
 cd ..
-zip -r distributions/intellij-mocha-mouse-dark-theme-$1.zip.tmp intellij-mocha-mouse-dark-theme
-rm -f distributions/intellij-mocha-mouse-dark-theme-$1.zip
-mv distributions/intellij-mocha-mouse-dark-theme-$1.zip.tmp distributions/intellij-mocha-mouse-dark-theme-$1.zip
+zip -r distributions/intellij-mocha-mouse-light-theme-$1.zip.tmp intellij-mocha-mouse-light-theme
+rm -f distributions/intellij-mocha-mouse-light-theme-$1.zip
+mv distributions/intellij-mocha-mouse-light-theme-$1.zip.tmp distributions/intellij-mocha-mouse-light-theme-$1.zip
